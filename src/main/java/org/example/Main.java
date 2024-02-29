1ac3c22528ae8fcf632ee367ba9d87a247b092a0
oregoniz <123117802+oregoniz@users.noreply.github.com>
2024-02-29 18:33:32 +0300

    2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,7 @@
 package org.example;
 
-import java.io.IOException;
-
-import static org.example.FileReader.getStudArr;
-import static org.example.FileReader.getUnivArr;
-import static org.example.StudyProfile.*;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
 
         FileReader.fileRead("universityInfo.xlsx");
         System.out.println(FileReader.studentsArr);
